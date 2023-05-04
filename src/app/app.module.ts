4944b8a206ddfe56deee403204629cbd8dd59d7c
TilaCl <man.diaza@duocuc.cl>
2023-05-03 20:32:01 -0400

    Commit 1
    Se agrega src BASE

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { HttpClientModule} from '@angular/common/http';
+
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers } from '@ionic/storage';
+
+import { QRCodeModule } from 'angularx-qrcode';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, QRCodeModule,IonicModule.forRoot(), AppRoutingModule,HttpClientModule,
+            IonicStorageModule.forRoot({
+              name:'mydb',
+              driverOrder:[Drivers.IndexedDB,Drivers.LocalStorage]
+            })],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+
+
+export class AppModule {}
